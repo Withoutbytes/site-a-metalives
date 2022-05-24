7d8e35aa90aa54beaf188bd5cc0335fb5ebec3d8
Jonathan <41968795+Withoutbytes@users.noreply.github.com>
2022-05-24 10:08:06 -0300

    docs: improved documentation, and added readme.md

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,23 @@
-import Link from "next/link";
+import { useEffect } from "react";
 import Layout from "../components/Layout";
 
 const IndexPage = () => {
+	// Site A -> Site B
 	const onClickProductSku = (sku: string) => {
 		const windowTop = window.top;
 		if (!windowTop) return;
 		windowTop.postMessage({ type: "ON_CLICK_PRODUCT", sku }, "*");
 	};
+
+	// Site B -> Site A
+	useEffect(() => {
+		window.addEventListener("message", (ev) => {
+			if (ev.data.type === "HELLO") {
+				console.log(ev.data.message);
+			}
+		});
+	}, []);
+
 	return (
 		<Layout title="Site A Metalives">
 			<h1 className="w-full mx-auto my-5 text-2xl font-bold text-center text-orange-500">Site A Metalives</h1>
